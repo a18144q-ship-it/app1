5cfa7fe16b9ffbced72dbd290461cbc00aa05be4
a18144q-ship-it <a18144q@gmail.com>
2026-03-23 21:20:26 +0800

    Delete designer-workspace directory

--- designer-workspace/android/app/src/main/java/com/life/record/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.life.record;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- designer-workspace/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.life.record',
-  appName: 'MyLifeApp',
-  webDir: 'dist'
-};
-
-export default config;
